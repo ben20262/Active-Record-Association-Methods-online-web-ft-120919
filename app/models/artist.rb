e008f612db8a10a1dc28e1e0fac01e0f5b005131
Benjamin Small <ben20262@aim.com>
2020-01-30 15:20:27 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,16 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    @songs.first.genre
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
-    @songs.size
+    self.songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    @genres.size
+    self.genres.size
   end
 end
